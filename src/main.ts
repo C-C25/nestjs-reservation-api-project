4c0e93125316ea738c85715f7ff6ec2e729142b0
C-C25 <rc012204@icloud.com>
2026-06-22 15:28:40 +0900

    feat: add Swagger API documentation

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -15,6 +16,16 @@ async function bootstrap() {
       forbidNonWhitelisted: true,
     }),
   );
+
+  const config = new DocumentBuilder()
+    .setTitle('소규모 프라이빗 운동 공간 예약 서비스')
+    .setDescription('NestJS 기반 예약 서비스 API 문서')
+    .setVersion('1.0')
+    .addBasicAuth()
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
 
   await app.listen(process.env.PORT ?? 3000);
 }
